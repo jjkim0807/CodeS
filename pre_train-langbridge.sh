5fcc33dd8184a67580c172a457c7f6d6d306fbc2
Jaejin Kim <jaejin.kim@snu.ac.kr>
2024-05-10 06:35:19 +0000

    create pretrain shell script for langbridge

---
+++ pre_train-langbridge.sh
@@ -0,0 +1,3 @@
+set -e
+
+accelerate launch train_causal_lm.py --per_device_train_batch_size 2 --block_size 8192 --seed 42 --pretrained_model_name_or_path kaist-ai/codellama-langbridge-9b --epochs 1 --lr 5e-5 --warmup_ratio 0.0 --checkpointing_steps 500 --tensorboard_log_dir ./train_logs/pt-langbridge-9b --mode pt --output_ckpt_dir ./ckpts/pt-langbridge-9b --pt_data_dir ./data/tokenized_corpus.bin
